2045af999829e5853d3d33b5401c0a3f177176f7
fowesn <fowesn@bk.ru>
2017-12-04 23:50:23 +0300

    оставлены только рекорды

--- Calculator/CalculatorTests/UnitTest1.cs
+++ Calculator/CalculatorTests/UnitTest1.cs
@@ -5,34 +5,7 @@ using Calculator;
 namespace CalculatorTests
 {
     [TestClass]
-    public class InitialState_Screen_Tests
-    {
-        [TestMethod]
-        public void Test()
-        {
-        }
-    }
-
-    [TestClass]
     public class CurrentState_Record_Tests
-    {
-        [TestMethod]
-        public void Test()
-        {
-        }
-    }
-
-    [TestClass]
-    public class CurrentState_Screen_Tests
-    {
-        [TestMethod]
-        public void Test()
-        {
-        }
-    }
-
-    [TestClass]
-    public class CurrentState_Record_Screen_Tests
     {
         [TestMethod]
         public void Test()
@@ -50,43 +23,7 @@ namespace CalculatorTests
     }
 
     [TestClass]
-    public class History_Screen_Tests
-    {
-        [TestMethod]
-        public void Test()
-        {
-        }
-    }
-
-    [TestClass]
-    public class History_Record_Screen_Tests
-    {
-        [TestMethod]
-        public void Test()
-        {
-        }
-    }
-
-    [TestClass]
     public class Planning_Record_Tests
-    {
-        [TestMethod]
-        public void Test()
-        {
-        }
-    }
-
-    [TestClass]
-    public class Planning_Screen_Tests
-    {
-        [TestMethod]
-        public void Test()
-        {
-        }
-    }
-
-    [TestClass]
-    public class Planning_Record_Screen_Tests
     {
         [TestMethod]
         public void Test()
