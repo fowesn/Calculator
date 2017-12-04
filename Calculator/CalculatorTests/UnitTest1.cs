3f66b820a41f25cac4f63450f24b885d8e75e1aa
fowesn <fowesn@bk.ru>
2017-12-04 20:29:23 +0300

    шаблон тестов

---
+++ Calculator/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class InitialState_Screen_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+
+    [TestClass]
+    public class CurrentState_Record_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+
+    [TestClass]
+    public class CurrentState_Screen_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+
+    [TestClass]
+    public class CurrentState_Record_Screen_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+
+    [TestClass]
+    public class History_Record_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+
+    [TestClass]
+    public class History_Screen_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+
+    [TestClass]
+    public class History_Record_Screen_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+
+    [TestClass]
+    public class Planning_Record_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+
+    [TestClass]
+    public class Planning_Screen_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+
+    [TestClass]
+    public class Planning_Record_Screen_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
